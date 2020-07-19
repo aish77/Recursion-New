3aa017f67047830dfc315364ab5095441ac391b5
aish77 <aishwary.mhjn@gmail.com>
2020-07-19 11:02:00 +0530

    Recursion codes

---
+++ src/PermuationsWithRandomCaseChangeAndLetters.java
@@ -0,0 +1,15 @@
+public class PermuationsWithRandomCaseChangeAndLetters {
+    public static void main(String[] args) {
+        String input = "A1b2";
+        String output = "";
+        solvePermutationsWithRandomCaseChangeAndLetters(input, output);
+    }
+
+    private static void solvePermutationsWithRandomCaseChangeAndLetters(String input, String output) {
+        if(input.length() == 0){
+
+        }
+    }
+
+
+}
